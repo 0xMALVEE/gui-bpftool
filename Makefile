b91b22b802960844c4721e69b3e1e28ea7a2deaf
M Alvee <m.alvee8141@gmail.com>
2025-04-26 10:58:06 +0600

    install-fyne

--- Makefile
+++ Makefile
@@ -2,20 +2,8 @@ APP_NAME = gui-bpftool
 BUILD_DIR = build/bin
 EXECUTABLE = $(BUILD_DIR)/$(APP_NAME)
 
-.PHONY: all build run clean
+.PHONY: go
 
-all: build run
-
-dev: build run
 
 go:
-	go build && sudo ./gui-bpftool
-
-build:
-	wails build -devtools -tags debug
-
-run: build
-	sudo $(EXECUTABLE)
-
-clean:
-	rm -rf $(BUILD_DIR)
+	go run .
--- main.go
+++ main.go
@@ -1,42 +1,19 @@
 package main
 
 import (
-	"embed"
+	"fmt"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
-//go:embed all:frontend/dist
-var assets embed.FS
-
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
+	fmt.Print("Hello World, First!\n")
+	a := app.New()
+	w := a.NewWindow("Hello World")
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "gui-bpftool",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+	fmt.Print("Hello World!\n")
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
-
-// func main() {
-// 	pinfo, _ := bpfprog.GetProgListWithInfo()
-
-// 	fmt.Printf("%+v\n", pinfo[0].Name)
-// }
